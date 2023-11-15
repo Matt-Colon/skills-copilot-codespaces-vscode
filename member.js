cd0b05a0d4ff18ca11583c99af6272a6eff9d283
Matt-Colon <141006457+Matt-Colon@users.noreply.github.com>
2023-11-15 16:48:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+  var member = {
+    name: 'John Doe',
+    age: 30,
+    skills: ['js', 'html', 'css']
+  };
+
+  var member2 = {
+    name: 'Jane Doe',
+    age: 25,
+    skills: ['js', 'html', 'css']
+  };
+
+  return {
+    member: member,
+    member2: member2
+  };
+}
